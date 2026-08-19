c49d84de08a6a1a290a50cb8426a2688393e26fc
Raiane Silva <53622364+raianesilvaz@users.noreply.github.com>
2026-08-19 00:39:33 -0300

    Remove ciclo de redirecionamento da entrada

--- worker/cloudflare.js
+++ worker/cloudflare.js
@@ -1,11 +1,5 @@
 export default {
   async fetch(request, env) {
-    const url = new URL(request.url);
-
-    if (url.pathname === "/") {
-      return Response.redirect(new URL("/index.html", url), 302);
-    }
-
     return env.ASSETS.fetch(request);
   },
 };
